966034f91fbe8e0e9338983f32e09d686851e054
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2026-08-27 17:58:06 +0700

    lanyard2

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <div className="container mx-auto min-h-screen px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-2 sm:gap-4 lg:grid-cols-12 lg:gap-0">
           <div className="relative h-[48vh] touch-none sm:h-[46vh] lg:col-span-6 lg:h-screen">
-            <Lanyard position={[0, 0, 12]} gravity={[0, -70, 0]} />
+            <Lanyard position={[0, 0, 12]} gravity={[0, -40, 0]} />
           </div>
           <div className="lg:col-span-6">
             <div className="flex h-full items-center py-0 sm:py-4 lg:py-0">
